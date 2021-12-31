494b3facdb155d49248d41e3a67b8111af65fa57
hykim <hykim22@gmail.com>
2021-12-31 15:14:52 +0900

    2번째 commit System.out.println("Hello, world! 999");

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
 	public static void main(String []args){
-		System.out.println("Hello, world!");
+		System.out.println("Hello, world! 999");
                             System.out.println("Hello, world!");//hostfix에서 작업중
 	}
 	
